72ab665267c3eb526a37303be891db3675a0b259
Yichi Zhang <ichicho@keio.jp>
2019-09-05 20:50:29 +0900

    add script for extraction of giftcodes from J's concier

---
+++ extract.py
@@ -0,0 +1,16 @@
+raw = open('raw_giftcodes.txt', 'r')
+target = open('.giftcodes', 'w')
+
+ans = 0
+cnt = 0
+
+while(True):
+    line = raw.readline()
+    if not line:
+        break
+    if (cnt % 4 == 0):
+        print(line, end='')
+        target.write(line)
+        ans += 1
+
+print(ans)
